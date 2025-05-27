0a2ed38f35081d0c517df353f4c7c22fc7296b41
farhad <farhadkh736@gmail.com>
2025-05-27 12:37:12 +0330

    Update Fraction_by_class.py

--- Fraction_by_class.py
+++ Fraction_by_class.py
@@ -30,10 +30,7 @@ class Fraction:
        result_d = self.denominator * f2.denominator
        
        x = Fraction(result_s,result_d)
-       return x   
-       
-   def fraction_to_number(self,b,d):
-       ...    
+       return x    
        
    def show(self):
        print(self.face, "/" , self.denominator)
@@ -48,26 +45,29 @@ b = int(input("enter number 2: "))
 c = int(input("enter number 3: "))
 d = int(input("enter number 4: "))
 
-aa = Fraction(a,b)
-aa.show()
+if b and d != 0:
+    aa = Fraction(a,b)
+    aa.show()
 
-bb = Fraction(c,d)
-bb.show()
+    bb = Fraction(c,d)
+    bb.show()
 
-print("if you want Multiplication:")
-show_Multiplication = aa.Multi(bb)
-show_Multiplication.show()
+    print("if you want Multiplication:")
+    show_Multiplication = aa.Multi(bb)
+    show_Multiplication.show()
 
-print("if you want Division:")
-answer_Division = aa.Div(bb)
-answer_Division.show()
+    print("if you want Division:")
+    answer_Division = aa.Div(bb)
+    answer_Division.show()
 
-print("if you want sum:")
-answer_sum = aa.sum(bb)
-answer_sum.show()
+    print("if you want sum:")
+    answer_sum = aa.sum(bb)
+    answer_sum.show()
 
-print("if you want minus:")
-answer_min = aa.minus(bb)
-answer_min.show()
+    print("if you want minus:")
+    answer_min = aa.minus(bb)
+    answer_min.show()
+else:
+    print("b and d cannot be 0.")
                        
                                                                                                
