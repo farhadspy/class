dd0563c0023780511f7de5060c8369d806f5c40a
farhad <farhadkh736@gmail.com>
2025-05-27 19:35:33 +0330

    Update time_by_class.py

--- time_by_class.py
+++ time_by_class.py
@@ -44,12 +44,29 @@ class Time:
         result = Time(new_h,new_m,new_s)
         return result
     
+    def gmt_to_tehran(self):
+        tehran_sum = self.sum(Time(3 , 30 , 0))
+        return tehran_sum
+        
+    def time_to_second(self):
+        seconds = self.hour * 3600 + self.minute * 60 + self.second
+        return seconds
+    
+    @staticmethod
+    def second_to_time(seconds):
+        hour = seconds // 3600
+        seconds = seconds - (hour * 3600)
+        minute =  seconds // 60
+        second = seconds - (minute * 60)
+        t = Time(hour , minute , second)
+        return t
+    
     def show(self):
         print(self.hour , ":" , self.minute , ":" , self.second)
 
 
 
-t1 = Time(3 , 45 , 17)
+t1 = Time(3 , 75 , 17)
 t1.show()
 
 t2 = Time(4 , 50 , 2)
@@ -60,3 +77,12 @@ t3.show()
 
 t4 = t1.min(t2)    
 t4.show()
+
+t5 = t3.gmt_to_tehran()
+t5.show() 
+
+t6 = t5.time_to_second()
+print(t6)
+
+t7 = Time.second_to_time(4000)
+t7.show()
